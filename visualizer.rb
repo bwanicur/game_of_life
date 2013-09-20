9952212be13e883279d415ab42922d22f72e2e13
ben wanicur <bwanicur@gmail.com>
2013-09-20 11:42:20 -0700

    not sure why, but iterating over the visualizer this way works for the initial seed file.  the original way does not work

--- visualizer.rb
+++ visualizer.rb
@@ -3,14 +3,21 @@ class Visualizer
     @@count = 1 
     
     def self.ascii(grid)
-        puts "#{@@count} ==========================="
-        grid.cols.times do |col|
-            grid.rows.times do |row|
-              print (grid.cell_at(row,col).alive? ? "*" : ".")
-            end
-         print "\n"
+      puts "#{@@count} ==========================="
+      #(0...grid.cols).each do |col|
+        #(0...grid.rows).each do |row|
+          #print (grid.cell_at(row,col).alive? ? "*" : ".")
+        #end
+       #print "\n"
+      #end
+      grid.grid.each do |row|
+        row.each do |cell|
+          print cell.alive? ? '*' : '.'
         end
-        puts
-        @@count +=1
+        print "\n"
+      end
+      puts
+      @@count +=1
     end
+
 end
